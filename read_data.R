44e9376b1d9e632c8eaaabfd39d671dff18ca088
Mirko Mustonen <mirkomustonen@gmail.com>
2019-11-01 11:16:32 +0200

    Created read_data.R script for reading data

--- read_data.R
+++ read_data.R
@@ -1,33 +1,41 @@
-files <- list.files('specdata', full.names = TRUE)
+# Create a function that takes as input the path to a single .csv file in the
+# specdata folder and creates a data table from its contents.
+# The data types in the output table have to match the data
+# (date is understood as a date).
 
+path_to <- 'specdata/002.csv'
 
-# for(file_i in files){
-#         data <- read.csv(file='specdata/001.csv')
-# }
+data_read_i <- function(path_to){
+        data_tab <- read.table(file = path_to, header = T, sep = ',', 
+                               colClasses = c('Date', 'numeric', 'numeric', 'factor'))        
+}
 
-# a <- read.csv(file='specdata/001.csv')
-# a <- read.table(file = 'specdata/001.csv', header = T, 
-#                 sep = ',', 
-#                 colClasses = c('Date', 'numeric', 'numeric', 'factor'))
-# k <- 0
-# for(file_i in files){
-#         data_i <- read.table(file = file_i, 
-#                         header = T, 
-#                         sep = ',', 
-#                         colClasses = c('Date', 'numeric', 'numeric', 'factor'))
-#         data_i2 <- data_i[!is.na(data_i$sulfate) | !is.na(data_i$nitrate), ]
-#         data_c <- data.frame(nr_lines_oa = nrow(data_i), 
-#                              nr_lines_noa = nrow(data_i2), 
-#                              ID = data_i$ID[1]) 
-#                              
-#         if(k == 0){
-#                 # data_l <- data_i2
-#                 data_l2 <- data_c
-#                 k <- 1
-#         }
-#         else{
-#                 # data_l <- rbind(data_l, data_i2)
-#                 data_l2 <- rbind(data_l2, data_c)
-#         }
-# }
+# Write a for loop that loops through all the file paths in the specdata 
+# folder and uses the function written in previous point to read the data from 
+# the .csv files. Create a table with columns that show: a) ID nr b) the nr of 
+# rows in each of the read in tables c) The number of rows where atleast 
+# sulfate or nitrate data is not missing.
 
+file_names <- list.files('specdata', full.names = TRUE)
+
+k <- 0
+for(file_i in file_names){
+        data_i <- data_read_i(file_i)
+        ID <- data_i$ID[1]
+        No_data_rows <- nrow(data_i)
+        No_rows_not_NA_sulf <- !is.na(data_i$sulfate)
+        No_rows_not_NA_nitr <- !is.na(data_i$nitrate)
+        No_rows_not_NA <- No_rows_not_NA_sulf | No_rows_not_NA_nitr
+        data_not_NA <- data_i[No_rows_not_NA,]
+        
+        No_rows_not_NA <- nrow(data_not_NA)
+        
+        out_tab_i <- data.frame(ID, No_data_rows, No_rows_not_NA)
+        if(k == 0){
+                out_tab <- out_tab_i
+                k <- 1
+        }else{
+                out_tab <- rbind(out_tab, out_tab_i)
+        }
+        
+}
