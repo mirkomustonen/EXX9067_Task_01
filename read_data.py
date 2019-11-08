9e16ffc627cde559e6ef7db8c949c4f4646b2aa1
Mirko Mustonen <mirkomustonen@gmail.com>
2019-11-08 18:51:25 +0200

    Worked on python and and jupyter notebooks versions of the solution

--- read_data.py
+++ read_data.py
@@ -6,6 +6,9 @@ Created on Fri Nov  1 11:51:30 2019
 @author: mirko
 """
 
+import pandas as pd
+from os import listdir, sep
+
 '''
 Create a function that takes as input the path to a single .csv file in the
 specdata folder and creates a data table from its contents.
@@ -13,23 +16,20 @@ The data types in the output table have to match the data
 (date is understood as a date).
 '''
 
-import pandas as pd
-
 path_to = 'specdata/003.csv'
 
+
 def data_read_i(path_to):
-    return pd.read_csv(path_to, parse_dates = [0])    
+    return pd.read_csv(path_to, parse_dates=[0])
 
 
 '''
-Write a for loop that loops through all the file paths in the specdata 
-folder and uses the function written in previous point to read the data from 
-the .csv files. Create a table with columns that show: a) ID nr b) the nr of 
-rows in each of the read in tables c) The number of rows where atleast 
+Write a for loop that loops through all the file paths in the specdata
+folder and uses the function written in previous point to read the data from
+the .csv files. Create a table with columns that show: a) ID nr b) the nr of
+rows in each of the read in tables c) The number of rows where atleast
 sulfate or nitrate data is not missing.
 '''
-
-from os import listdir, sep
 
 files_list = listdir('specdata')
 
@@ -42,13 +42,11 @@ for file_i in files_list:
     data_i = data_read_i(path_i)
     ID = data_i.ID[0]
     No_data_rows = len(data_i.ID)
-    data_not_NA = data_i.dropna(how = 'all', subset=['sulfate', 'nitrate'])
+    data_not_NA = data_i.dropna(how='all', subset=['sulfate', 'nitrate'])
     No_rows_not_NA = len(data_not_NA)
-    
     ID_vec.append(ID)
     No_data_rows_vec.append(No_data_rows)
     No_rows_not_NA_vec.append(No_rows_not_NA)
-    
-out_tab = pd.DataFrame({'ID': ID_vec, 
-                        'No_data_rows': No_data_rows_vec, 
+out_tab = pd.DataFrame({'ID': ID_vec,
+                        'No_data_rows': No_data_rows_vec,
                         'No_rows_not_NA': No_rows_not_NA_vec})
